9a1d5ea9b3ddb29d41186a9ab999683422dd0183
ubaldo <morganfrezze22@gmail.com>
2023-10-10 19:19:52 -0600

    changing ./ to ../ in file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap'
 import Formulario from './components/Formulario'
 import ListadoBebidas from './components/ListadoBebidas'
 import ModalBebida from './components/ModalBebida'
-import { CategoriasProvider } from "./context/CategoriasProvider.jsX"
+import { CategoriasProvider } from '../context/CategoriasProvider'
 import { BebidasProvider } from './context/BebidasProvider.jsx'
  
 
